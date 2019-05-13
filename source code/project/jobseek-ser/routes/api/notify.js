7ae21f455290f1b41c0e96a48940c0b641ef4b9c
balavignan <47048270+balavignan@users.noreply.github.com>
2019-05-13 14:34:04 -0500

    project

--- source code/project/jobseek-ser/routes/api/notify.js
+++ source code/project/jobseek-ser/routes/api/notify.js
@@ -5,10 +5,10 @@ const sgMail = require('@sendgrid/mail');
 
     signupNotification=function (req,res){
         console.log('email to sender',req);
-          sgMail.setApiKey('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');         const msg = {
+          sgMail.setApiKey('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');         const msg = {
             to:req,
-            from: 'chintareddy.sudhakarreddy@gmail.com',
-            subject: 'Jobnut signup greetings',
+            from: 'balavignan97@gmail.com',
+            subject: 'JobSeek signup greetings',
             html:`<h1>Hi user,</h1>
                    <h2>You are at right place we will provide good job offers</h2>`
           };
@@ -23,8 +23,8 @@ const sgMail = require('@sendgrid/mail');
         console.log('email to sender',req);
         sgMail.setApiKey('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');        const msg = {
             to:req,
-            from: 'chintareddy.sudhakarreddy@gmail.com',
-            subject: 'Jobnut apply notification',
+            from: 'balavignan97@gmail.com',
+            subject: 'JobSeek apply notification',
             html:`<h1>Hi user,</h1>
                    <h2> user application was submitted successfully job provider will contact user for further process.</h2>`
           };
@@ -37,10 +37,10 @@ const sgMail = require('@sendgrid/mail');
     }
     signupHrNotification=function (req,res){
         console.log('email to sender',req);
-        sgMail.setApiKey('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');        const msg = {
+        sgMail.setApiKey('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');        const msg = {
             to:req,
-            from: 'chintareddy.sudhakarreddy@gmail.com',
-            subject: 'Jobnut signup grettings for HR',
+            from: 'balavignan97@gmail.com',
+            subject: 'JobSeek signup grettings for HR',
             html:`<h1>Hi Job Provider,</h1>
                    <h2>Please upload some job offers in your account where user will apply for it</h2>`
           };
@@ -53,10 +53,10 @@ const sgMail = require('@sendgrid/mail');
     }
     passwordUpdateNotification=function (req,res){
         console.log('email to sender',req);
-          sgMail.setApiKey('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');         const msg = {
+          sgMail.setApiKey('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');         const msg = {
             to:req,
-            from: 'krishnasai511@gmail.com',
-            subject: 'Jobnut password updated',
+            from: 'balavignan97@gmail.com',
+            subject: 'JobSeek password updated',
             html:`<h1>Hello user,</h1>
                    <h2>As per your request your password was updated successfuly</h2>`
           };
